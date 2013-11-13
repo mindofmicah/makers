ca4e1d298ea9dde07ec54dac7b7c43a71c2a20e8
Micah <micah@whamdonk.com>
2013-11-13 15:46:30 -0600

    Builds the config file as well as the base Validator file

--- src/mindofmicah/Makers/Commands/ValidatorCommand.php
+++ src/mindofmicah/Makers/Commands/ValidatorCommand.php
@@ -3,6 +3,7 @@ namespace mindofmicah\Makers\Commands;
 
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem as File;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
@@ -21,6 +22,48 @@ class ValidatorCommand extends Command {
      * @var string
      */
     protected $description = 'Build a validator service.';
+
+    protected $file;
+    public function __construct(File $file) 
+    {
+        $this->file = $file;
+        parent::__construct();
+    }    
+    
+    public function fire()
+    {
+        if (!$this->file->exists(__DIR__ . '/../../../../config.json') ) {
+            $config = [];
+        } else {
+            $config = json_decode($this->file->get(__DIR__.'/config.json'));
+        }
+
+        if (!array_key_exists('namespace', $config)) {
+            $config['namespace'] = $this->ask('Hey, which namespace?');
+        }
+
+        $directory = app_path() . '/'.strtr($config['namespace'], '\\','/');
+        if (!$this->file->isDirectory($directory)) {
+            $this->file->makeDirectory($directory, 0775, true);
+        }
+
+        if (!$this->file->exists($directory . '/Validator.php')) {
+            $contents = str_replace('$namespace',$config['namespace'],$this->file->get(__DIR__.'/../Validator.txt'));
+            $this->file->put(
+                $directory . '/Validator.php', 
+                $contents);
+        }
+
+        // If there isn't a config file, we need to build one
+        // Set a flag that we'll need to save it
+        // If there is one, use it
+
+        //
+
+
+
+        $this->file->put(__DIR__.'/../../../../config.json', json_encode($config));
+    }
 
     /**
      * Get the console command arguments.
